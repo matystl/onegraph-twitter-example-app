22a69aa8b51bfc3a662c6cab776d59c6c5f6b77f
Matus Fedak <mfedak@gmail.com>
2018-03-06 19:36:19 +0100

    Added simple apollo integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import logo from "./logo.svg";
+import "./App.css";
+
+const GET_VIDEO = gql`
+  query {
+    youTubeVideo(id: "t6CRZ-iG39g") {
+      snippet {
+        title
+        description
+      }
+    }
+  }
+`;
+
+class VideoComponent extends Component {
+  render() {
+    return (
+      <Query query={GET_VIDEO}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Loading...</div>;
+          if (error) return <div>Uh oh, something went wrong!</div>;
+          return (
+            <div>
+              We fetched a YouTube video with title:{" "}
+              {data.youTubeVideo.snippet.title}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,9 +42,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <VideoComponent />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const APP_ID = "cbf75b15-4c10-4772-adb4-06fc19799d02";
+
+const client = new ApolloClient({
+  uri: "https://serve.onegraph.com/dynamic?app_id=" + APP_ID,
+  headers: { "Content-Type": "application/json" },
+  credentials: "include"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
