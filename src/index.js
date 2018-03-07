4676d2436f048e9c2caa9467842712e47bb17982
Daniel Woelfel <dwwoelfel@gmail.com>
2018-03-06 21:00:17 -0800

    use apollo-client directly instead of boost

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
 import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import "./index.css";
 import App from "./App";
 import LoginWrapper from "./LoginWrapper";
@@ -10,9 +12,12 @@ import registerServiceWorker from "./registerServiceWorker";
 const APP_ID = "cbf75b15-4c10-4772-adb4-06fc19799d02";
 
 const client = new ApolloClient({
-  uri: "https://serve.onegraph.com/dynamic?app_id=" + APP_ID,
-  headers: { "Content-Type": "application/json" },
-  credentials: "include"
+  link: new HttpLink({
+    uri: "https://serve.onegraph.com/dynamic?app_id=" + APP_ID,
+    headers: {"Content-Type": "application/json"},
+    credentials: "include",
+  }),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
